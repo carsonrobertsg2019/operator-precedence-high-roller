c6ebdf86a49665f8f59543f6960c62c06a9eb718
Carson <carsonrobertsg2019@gmail.com>
2024-05-31 17:24:43 -0500

    gitignore, base bot functionality

--- high_roller.py
+++ high_roller.py
@@ -24,3 +24,20 @@ def determine_operator_precedence(a, b):
     ]
 
     return operator_precedence_table[a][b]
+
+import discord
+from discord.ext import commands
+intents = discord.Intents.all()
+client = discord.Client(command_prefix='!', intents=intents)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if len(message.content) > 0 and message.content[0] == '!':
+        await message.channel.send(message.content)
+            
+with open('BOT-KEY', 'r') as file: bot_key = file.read().rstrip()
+client.run(bot_key)
