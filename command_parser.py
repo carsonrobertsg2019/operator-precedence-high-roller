5e1e3bad9e89a2d15c4e5e0287d38085e50e1f01
Carson <carsonrobertsg2019@gmail.com>
2024-06-02 18:25:09 -0500

    moved operator precedence table to parser.py

--- command_parser.py
+++ command_parser.py
@@ -6,6 +6,33 @@ class CommandParser:
         self.syntax_error = False
         self.lexer = LexicalAnalyzer()
 
+    def map_to_index(map_char):
+        if map_char == '+':         return 1
+        elif map_char == '-':       return 2
+        elif map_char == '*':       return 3
+        elif map_char == '/':       return 4
+        elif map_char == '(':       return 5
+        elif map_char == ')':       return 6
+        elif map_char == 'R':       return 7 #ROLL
+        elif map_char == 'N':       return 8 #NUM
+        elif map_char == '$':       return 9
+        else:                       return -1 #force an index out of bounds
+
+    def determine_operator_precedence(a, b):
+        operator_precedence_table = [
+            ['?', '+', '-', '*', '/', '(', ')', 'R', 'N', '$'], 
+            ['+', '>', '>', '<', '<', '<', '>', '<', '<', '>'],
+            ['-', '>', '>', '<', '<', '<', '>', '<', '<', '>'],
+            ['*', '>', '>', '>', '>', '<', '>', '<', '<', '>'],
+            ['/', '>', '>', '>', '>', '<', '>', '<', '<', '>'],
+            ['(', '<', '<', '<', '<', '<', '=', '<', '<', 'X'],
+            [')', '>', '>', '>', '>', 'X', '>', 'X', 'X', '>'],
+            ['R', '>', '>', '>', '>', 'X', '>', 'X', 'X', '>'],
+            ['N', '>', '>', '>', '>', 'X', '>', 'X', 'X', '>'],
+            ['$', '<', '<', '<', '<', '<', 'X', '<', '<', 'A']
+        ]
+        return operator_precedence_table[a][b]
+
     def expect(self, expected_type):
         token = self.lexer.get_token()
         if token.TokenType != expected_type:
--- high_roller.py
+++ high_roller.py
@@ -1,30 +1,3 @@
-def map_to_index(map_char):
-    if map_char == '+':         return 1
-    elif map_char == '-':       return 2
-    elif map_char == '*':       return 3
-    elif map_char == '/':       return 4
-    elif map_char == '(':       return 5
-    elif map_char == ')':       return 6
-    elif map_char == 'R':       return 7 #ROLL
-    elif map_char == 'N':       return 8 #NUM
-    elif map_char == '$':       return 9
-    else:                       return -1 #force an index out of bounds
-
-def determine_operator_precedence(a, b):
-    operator_precedence_table = [
-        ['?', '+', '-', '*', '/', '(', ')', 'R', 'N', '$'], 
-        ['+', '>', '>', '<', '<', '<', '>', '<', '<', '>'],
-        ['-', '>', '>', '<', '<', '<', '>', '<', '<', '>'],
-        ['*', '>', '>', '>', '>', '<', '>', '<', '<', '>'],
-        ['/', '>', '>', '>', '>', '<', '>', '<', '<', '>'],
-        ['(', '<', '<', '<', '<', '<', '=', '<', '<', 'X'],
-        [')', '>', '>', '>', '>', 'X', '>', 'X', 'X', '>'],
-        ['R', '>', '>', '>', '>', 'X', '>', 'X', 'X', '>'],
-        ['N', '>', '>', '>', '>', 'X', '>', 'X', 'X', '>'],
-        ['$', '<', '<', '<', '<', '<', 'X', '<', '<', 'A']
-    ]
-    return operator_precedence_table[a][b]
-
 import discord
 from discord.ext import commands
 intents = discord.Intents.all()
