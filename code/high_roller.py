a44a77d3315f30e0d27543b11a751c9581671684
Carson <carsonrobertsg2019@gmail.com>
2024-06-17 21:49:18 -0500

    more changes

--- code/high_roller.py
+++ code/high_roller.py
@@ -15,21 +15,24 @@ async def on_message(message):
         p = CommandParser(message.content)
         p.parse_init()
         c = Compute()
-        if len(p.stack) < 2 or p.syntax_error or c.error:
-            await message.channel.send('https://tenor.com/view/blm-gif-25815938')    
+        if len(p.stack) < 2 or p.syntax_error:
+            await message.channel.send('https://tenor.com/view/blm-gif-25815938')
         else:
             result = c.compute_expr(p.stack[1])
-            to_send = ""
-            i = 0
-            for cock in c.cocked_rolls:
-                to_send += "Honor the cock. Roll " + str(c.cocked_rolls[i][1]) + " was cocked. It would have been " + str(c.cocked_rolls[i][0]) + "\n"
-            to_send += str(result) + '\nDetails: ' + str(c.all_lists_of_rolls)
-            if (len(to_send)) > 2000:
-                to_send_list = [(to_send[i:i+2000]) for i in range(0, len(to_send), 2000)]
-                for to_send in to_send_list:
-                    await message.channel.send(to_send)
+            if c.error:
+                await message.channel.send('https://tenor.com/view/blm-gif-25815938')
             else:
-                await message.channel.send(to_send)            
+                to_send = ""
+                i = 0
+                for cock in c.cocked_rolls:
+                    to_send += "Honor the cock. Roll " + str(c.cocked_rolls[i][1]) + " was cocked. It would have been " + str(c.cocked_rolls[i][0]) + "\n"
+                to_send += str(result) + '\nDetails: ' + str(c.all_lists_of_rolls)
+                if (len(to_send)) > 2000:
+                    to_send_list = [(to_send[i:i+2000]) for i in range(0, len(to_send), 2000)]
+                    for to_send in to_send_list:
+                        await message.channel.send(to_send)
+                else:
+                    await message.channel.send(to_send)            
         
 with open('BOT-KEY', 'r') as file: bot_key = file.read().rstrip()
 client.run(bot_key)
