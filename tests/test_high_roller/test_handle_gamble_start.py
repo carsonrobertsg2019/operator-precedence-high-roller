35d18fe459951c368ebed7961a6d339b5433340f
Carson <carsonrobertsg2019@gmail.com>
2024-08-22 21:45:00 -0500

    bashing my head against the wall until the wall breaks

--- tests/test_high_roller/test_handle_gamble_start.py
+++ tests/test_high_roller/test_handle_gamble_start.py
@@ -1,5 +1,11 @@
-from src.operator_precedence_high_roller.high_roller import HighRoller
+import pathlib, sys, os
+ROOT_PATH = pathlib.Path(__file__).parents[2]
+sys.path.append(os.path.join(ROOT_PATH, 'src'))
+print(sys.path)
+from operator_precedence_high_roller_bot import high_roller
 
-hr = HighRoller()
-
-#work in progress :)
+"""class TestHandleGambleStart(IsolatedAsyncioTestCase):
+    async def test_not_gambling_true(self):
+        print('HAHAHA')
+        await high_roller.handle_gamble_bet(None, None)
+        self.assertTrue(True)"""
